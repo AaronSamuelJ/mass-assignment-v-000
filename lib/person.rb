6ae64ba1a084d0eb0933b4682ead4a15ac80dbd2
Aaron Samuel <aasamuelj@gmail.com>
2017-10-07 21:21:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
   def initialize(attributes)
-    attributes.each do |key, value|
+    attributes.each do |key, value| # => accepts an argument of a hash, iterates through it to set each value to the key method
       self.send(("#{key}="), value)
     end
   end
